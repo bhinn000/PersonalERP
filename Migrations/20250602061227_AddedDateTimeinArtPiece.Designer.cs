310bc5c3a818a21ea3f7a91099069f0b320f869a
Bhintuna <bhintunash@gmail.com>
2025-06-02 12:10:55 +0545

    Added Migration for DateTime in ArtPiece

--- Migrations/20250602061227_AddedDateTimeinArtPiece.Designer.cs
+++ Migrations/20250602061227_AddedDateTimeinArtPiece.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PersonalERP;
 
@@ -11,9 +12,11 @@ using PersonalERP;
 namespace PersonalERP.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250602061227_AddedDateTimeinArtPiece")]
+    partial class AddedDateTimeinArtPiece
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20250602061227_AddedDateTimeinArtPiece.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonalERP.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedDateTimeinArtPiece : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "ArtPieces",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "ArtPieces",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeletedBy",
+                table: "ArtPieces",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedDate",
+                table: "ArtPieces",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "ArtPieces",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "ArtPieces",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "ArtPieces");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "ArtPieces");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedBy",
+                table: "ArtPieces");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedDate",
+                table: "ArtPieces");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "ArtPieces");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "ArtPieces");
+        }
+    }
+}
